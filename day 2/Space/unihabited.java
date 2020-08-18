09f829176c76f4a4ef4754149187d67517959c99
oreol3003 <67114580+oreol3003@users.noreply.github.com>
2020-08-18 19:51:21 +0300

    Update unihabited.java

--- day 2/Space/unihabited.java
+++ day 2/Space/unihabited.java
@@ -2,33 +2,4 @@ package Space;
 
 // Нежилые помещения
 
-public  abstract class unihabited extends realEstateObject {
-    private int area;
-    private int price;
-    private int total_price;
-
-
-    public int getArea() {
-        return area;
-    }
-
-    public void setArea(int area) {
-        this.area = area;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getTotal_price() {
-        return total_price;
-    }
-
-    public void setTotal_price(int total_price) {
-        this.total_price = total_price;
-    }
-}
+public abstract class Unihabited extends RealEstateObject { }
