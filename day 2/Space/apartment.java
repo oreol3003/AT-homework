bec8ce85a34f7a54e4c6af4fa97042819241a9a9
oreol3003 <67114580+oreol3003@users.noreply.github.com>
2020-08-18 19:45:25 +0300

    Update apartment.java

--- day 2/Space/apartment.java
+++ day 2/Space/apartment.java
@@ -2,36 +2,4 @@ package Space;
 
 // Данные по квартире
 
-public class apartment extends residential {
-
-    private int area;           //Площадь
-    private int price;          //Цена за 1 м^2
-    private int total_price;    //Полная цена
-
-
-    public void setArea(int area) {
-        this.area = area;
-    }
-
-    public int getArea() {
-        return area;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setTotal_price(int total_price) {
-        this.total_price = total_price;
-    }
-
-    public int getTotal_price() {
-        return total_price;
-    }
-
-
-}
+public class Apartment extends Residential { }
