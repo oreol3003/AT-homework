51e30b0cf6f0835cfac9229fc1f07ef5a85f1c97
oreol3003 <67114580+oreol3003@users.noreply.github.com>
2020-08-18 19:50:50 +0300

    Update residential.java

--- day 2/Space/residential.java
+++ day 2/Space/residential.java
@@ -1,35 +1,5 @@
 package Space;
 
-// Жилое помещение
+// Жилые помещения
 
-public abstract class residential extends realEstateObject {
-
-    private int area;
-    private int price;
-    private int total_price;
-
-
-        public int getArea() {
-        return area;
-    }
-
-    public void setArea(int area) {
-        this.area = area;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getTotal_price() {
-        return total_price;
-    }
-
-    public void setTotal_price(int total_price) {
-        this.total_price = total_price;
-    }
-}
+public abstract class Residential extends RealEstateObject { }
